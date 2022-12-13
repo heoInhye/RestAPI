d387fe95f606b914472e6ece0b2022d7015d9aab
heojane <pepupx2@naver.com>
2022-12-13 21:37:31 +0900

    EventContoller

---
+++ src/main/java/com/jane/restapi/events/EventController.java
@@ -0,0 +1,43 @@
+package com.jane.restapi.events;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Controller
+@RequestMapping(value="/api/events/", produces = MediaTypes.HAL_JSON_VALUE)
+public class EventController {
+
+
+  @PostMapping
+  public ResponseEntity createEvent(@RequestBody Event event) {
+    //created()로 보낼 때 uri 필요
+    URI createdUri = linkTo(EventController.class)
+                    .slash("{id}")
+                    .toUri();
+    event.setId(100);
+    return ResponseEntity.created(createdUri).body(event);
+  }
+
+
+//  @PostMapping("/api/events/")
+//  public ResponseEntity createEvent() {
+//    //created()로 보낼 때 uri 필요
+//    URI createdUri =
+//    linkTo( methodOn(EventController.class).createEvent() )
+//            .slash("{id}")
+//            .toUri();
+//
+//    return ResponseEntity.created(createdUri).build();
+//  }
+
+
+}
--- src/test/java/com/jane/restapi/events/EventControllerTest.java
+++ src/test/java/com/jane/restapi/events/EventControllerTest.java
@@ -1,5 +1,6 @@
 package com.jane.restapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,6 +10,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest //@Controller, @RestController가 설정된 클래스를 메모리에 생성
@@ -17,14 +20,25 @@ class EventControllerTest {
   @Autowired
   MockMvc mockMvc; //서블릿 컨테이너를 모킹
 
+  @Autowired
+  ObjectMapper objectMapper; //json타입으로 손쉽게 변환가능
+
   @Test
   void createEvent() throws Exception {
+    Event event = Event.builder()
+                       .name("spring")
+                       .build();
+
     mockMvc.perform(
               post("/api/events/")
               .contentType(MediaType.APPLICATION_JSON)
               .accept(MediaTypes.HAL_JSON)
+              .content(objectMapper.writeValueAsString(event))
             )
-            .andExpect(status().isCreated());
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("id").exists())
+            ;
   }
 
 
